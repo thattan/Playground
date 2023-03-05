218d619dac14d034dd6aacf7c951b4e64342b20c
Tyler Hattan <tylerhattan@yahoo.com>
2023-03-05 00:25:02 -0600

    identity is kinda working

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Playground.Data;
+
+[assembly: HostingStartup(typeof(Playground.Areas.Identity.IdentityHostingStartup))]
+namespace Playground.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<PlaygroundContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("PlaygroundContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<PlaygroundContext>();
+            });
+        }
+    }
+}
